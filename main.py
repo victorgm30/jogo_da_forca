69ba7731dd3c9da4aa624ece433a1153db07fcb9
VictorGM <victorg.moreira92@gmail.com>
2025-07-28 08:06:26 -0300

    Created Database.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import jogo as j
+import bd
 
 def mostrar_menu():
     print("="*30)
@@ -10,6 +11,8 @@ def mostrar_menu():
     print("="*30)
 
 while True:
+    conn = bd.conectar()
+    bd.criar_tabela(conn)
     mostrar_menu()
 
     opcao = int(input('Escolha uma opção (1/2/3): '))
@@ -20,7 +23,17 @@ while True:
         input('Digite qualquer tecla para continuar...')
 
     elif opcao == 2:
-        print('Mostrar score!')
+        print('SCORE: ')
+        dados = bd.listar_dados()
+        if not dados:
+            print('Score vazio.')
+        else:
+            i = 1
+            for jogador in dados:
+                print(f'{i} -> {jogador[1]}, Pontuação: {jogador[2]}')
+                i +=1
+
+        input('Digite qualquer tecla para continuar...')
 
     elif opcao == 3:
         print('Saindo do jogo. Até mais!')
@@ -28,3 +41,5 @@ while True:
 
     else:
         print('Opção inválida! Tente novamente!')
+
+bd.desconectar(conn)
